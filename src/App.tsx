879b8367570b1dfbf51be7de9048f93ecacfe168
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-03-23 12:31:26 +0100

    Initial commit with Vite and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="flex h-screen items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white text-3xl font-bold">
+          Hello Julcia
+      </p>
+    </>
+  )
+}
+
+export default App
