679d095366d6b2c39924eff1983ac39cda4ae54c
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-11 19:59:57 +0200

    Adding Refresh&Filter&Modal Providers to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import DataProvider from "./providers/DataProvider.tsx";
 import AuthProvider from "./providers/AuthProvider.tsx";
+import RefreshProvider from "./providers/RefreshProvider.tsx";
+import FilterProvider from "./providers/FilterProvider.tsx";
+import ModalProvider from "./providers/ModalProvider.tsx";
 import AppRoutes from "./routes";
 
 function App() {
@@ -10,7 +13,13 @@ function App() {
       <HelmetProvider>
         <AuthProvider>
           <DataProvider>
-            <AppRoutes />
+            <RefreshProvider>
+              <FilterProvider>
+                <ModalProvider>
+                  <AppRoutes />
+                </ModalProvider>
+              </FilterProvider>
+            </RefreshProvider>
           </DataProvider>
         </AuthProvider>
       </HelmetProvider>
