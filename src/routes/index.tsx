78429949df6831154d83af5f01eddd21c9b6d78a
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-14 20:47:44 +0200

    Adding index.tsx with AppRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,18 @@
 import { useRoutes } from "react-router-dom";
 import LandingPage from "../pages/anon/LandingPage.tsx";
+import AnonRouter from "./Routers/AnonRouter.tsx";
+import AdminRouter from "./Routers/AdminRouter.tsx";
+import UserRouter from "./Routers/UserRouter.tsx";
 
 export default function AppRoutes() {
-  const commonRouter = [{ path: "/", element: <LandingPage /> }];
+  // const { isAuthenticated, user } = useAuth();
 
-  return useRoutes(commonRouter);
+  // if (!isAuthenticated) {
+  //   return <AnonRouter />;
+  // } else if (user?.role === "admin") {
+  //   return <AdminRouter />;
+  // }
+  return <UserRouter />;
+
+  // return useRoutes(commonRouter);
 }
