8658c496bc25d8f6063476a545ff86c829505597
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-16 23:14:00 +0200

    Update of api.tsx

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -4,13 +4,16 @@ const api = axios.create({
   baseURL: "http://localhost:5000",
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 api.interceptors.response.use(
   (response) => response,
