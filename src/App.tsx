f93f389049ecf0fd91cebb2201d1b40aaedbd2cb
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-22 22:57:19 +0200

    Changing path to alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import DataProvider from "./providers/DataProvider.tsx";
-import AuthProvider from "./providers/AuthProvider.tsx";
-import RefreshProvider from "./providers/RefreshProvider.tsx";
-import FilterProvider from "./providers/FilterProvider.tsx";
-import ModalProvider from "./providers/ModalProvider.tsx";
-import AppRoutes from "./routes";
+import DataProvider from "@/providers/DataProvider.tsx";
+import AuthProvider from "@/providers/AuthProvider.tsx";
+import RefreshProvider from "@/providers/RefreshProvider.tsx";
+import FilterProvider from "@/providers/FilterProvider.tsx";
+import ModalProvider from "@/providers/ModalProvider.tsx";
+import AppRoutes from "@/routes";
 
 function App() {
   return (
