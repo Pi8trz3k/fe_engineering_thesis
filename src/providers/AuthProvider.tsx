4403e4086e60010964536f4d65e10852edca904e
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-22 17:37:45 +0200

    Adding isAuthenticated and role parameter to AuthProvider. Changing function that checks if accessToken is valid. Changin logIn function(response variable adjustments) Adding register function

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { jwtDecode, JwtPayload } from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import api from "../lib/api.tsx";
 
 interface AuthContextType {
@@ -16,6 +16,7 @@ interface AuthContextType {
     email: string,
     password: string,
   ) => Promise<void>;
+  isAuthenticated: boolean;
 }
 
 export const AuthContext = createContext<AuthContextType>({
@@ -25,6 +26,7 @@ export const AuthContext = createContext<AuthContextType>({
   logIn: async () => {},
   logOut: () => {},
   register: async () => {},
+  isAuthenticated: false,
 });
 
 export default function AuthProvider({ children }: { children: ReactNode }) {
@@ -34,45 +36,52 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
   const [refreshToken, setRefreshToken] = useState<string | null>(
     localStorage.getItem("refreshToken"),
   );
+  const [role, setRole] = useState<string>("anon");
   const navigate = useNavigate();
 
-  const isTokenExpired = () => {
-    if (!accessToken) return false;
-
-    try {
-      const decoded = jwtDecode<JwtPayload>(accessToken);
-
-      const currentTime = Math.floor(Date.now() / 1000);
-      return decoded.exp ? decoded.exp > currentTime : false;
-    } catch (error) {
-      console.error("Invalid token", error);
-      return false;
-    }
-  };
-
   useEffect(() => {
-    if (accessToken && !isTokenExpired) {
-      logOut();
+    if (accessToken) {
+      try {
+        const decoded = jwtDecode<{ role: string }>(accessToken);
+        const currentTime = Math.floor(Date.now() / 1000);
+
+        console.log("decoded: ", decoded);
+
+        if (decoded.exp < currentTime) {
+          logOut();
+        } else {
+          setRole(decoded.role);
+        }
+      } catch (error) {
+        console.log("Invalid token: ", accessToken, ", error: ", error);
+        logOut();
+      }
     }
   }, []);
 
   const logIn = async (email: string, password: string) => {
     try {
-      const formData = new FormData();
-      formData.set("email", email);
-      formData.set("password", password);
+      const formData = new URLSearchParams();
+      formData.append("username", email); // important: `username`, not `email`
+      formData.append("password", password);
 
-      const response = await api.post("/token/login", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
+      const response = await api.post("/token/login", formData.toString(), {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
       });
 
       const userAccessToken = response.data.access_token;
+      const refreshToken = response.data.refresh_token;
+
       setAccessToken(userAccessToken);
       localStorage.setItem("accessToken", userAccessToken);
 
-      const refreshToken = response.data.refresh_token;
       setRefreshToken(refreshToken);
       localStorage.setItem("refreshToken", refreshToken);
+
+      const decoded = jwtDecode<{ role: string }>(userAccessToken);
+      setRole(decoded.role);
+
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
@@ -83,16 +92,53 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
     localStorage.removeItem("accessToken");
     setRefreshToken(null);
     localStorage.removeItem("refreshToken");
+    setRole("anon");
+
     navigate("/");
   };
 
+  //TODO
+  /*
+  Trzeba dodać obsługe trenera, czyli jakby typ użytkownika który jest rejestrowany
+  Nie wiem czy będzie to dodanie zwykłego type: string do parametrów ale wiadomo
+   */
+  const register = async (
+    name: string,
+    last_name: string,
+    email: string,
+    phoneNumber: string,
+    password: string,
+  ) => {
+    try {
+      const formData = new FormData();
+      formData.set("name", name);
+      formData.set("last_name", last_name);
+      formData.set("email", email);
+      formData.set("phone_number", phoneNumber);
+      formData.set("password", password);
+
+      await api.post("/user/", formData);
+
+      navigate("/");
+    } catch (error) {
+      console.log("Nie udało się zarejestrować: ", error);
+    }
+  };
   useEffect(() => {
     console.log("AuthProvider");
   }, []);
 
   return (
     <AuthContext.Provider
-      value={{ accessToken, refreshToken, role, logIn, logOut, register }}
+      value={{
+        accessToken,
+        refreshToken,
+        role,
+        logIn,
+        logOut,
+        register,
+        isAuthenticated: !!accessToken,
+      }}
     >
       {children}
     </AuthContext.Provider>
