a8ca285afab8bedfed5e0aada9b2228e3489ce33
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-28 00:56:30 +0200

    Changing AnonRouter, adding AnonLayout, Login and Register test pages

---
+++ src/pages/anon/LoginPage.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <div className={"text-black dark:text-white dark:bg-gray-800"}>
+        Login Page
+      </div>
+    </div>
+  );
+}
