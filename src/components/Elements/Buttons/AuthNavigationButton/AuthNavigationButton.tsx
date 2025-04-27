3c82d115c7479dffaf4b55063d1d27a779df3832
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-27 20:46:53 +0200

    Adding variants in AuthNavigationButton and removing few parameters

--- src/components/Elements/Buttons/AuthNavigationButton/AuthNavigationButton.tsx
+++ src/components/Elements/Buttons/AuthNavigationButton/AuthNavigationButton.tsx
@@ -1,23 +1,24 @@
 interface AuthNavigationButtonProps {
-  text: string;
-  color: string;
-  bgColor: string;
+  text: "Login" | "Register";
   onClick?: () => void;
-  className?: string;
 }
 
 export function AuthNavigationButton({
   text,
-  color,
-  bgColor,
   onClick,
-  className,
 }: AuthNavigationButtonProps) {
+  const variants = {
+    Login:
+      "text-black dark:text-white bg-white dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 dark:border dark:border-black",
+    Register:
+      "text-white dark:text-black bg-gray-800 dark:bg-white hover:bg-gray-700 dark:hover:bg-gray-200",
+  };
+
   return (
     <button
       onClick={onClick}
-      className={`${color} ${bgColor} px-7 py-3 rounded-sm font-semibold shadow-lg transition-all duration-300 transform 
-      hover:scale-105 active:scale-95 ${className}`}
+      className={`px-7 py-3 rounded-sm font-semibold shadow-lg transition-all duration-300 transform hover:scale-105 
+      active:scale-95 ${variants[text]} `}
     >
       {text}
     </button>
