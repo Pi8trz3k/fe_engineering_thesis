5c7f2abe206c1503555e0409e3a7f5e6297fbeb3
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-04-29 15:54:34 +0200

    ThemeToggle update of colors

--- src/components/Elements/Theme/ThemeToggle.tsx
+++ src/components/Elements/Theme/ThemeToggle.tsx
@@ -22,7 +22,7 @@ export default function ThemeToggle() {
     <button
       onClick={toggleTheme}
       className={`w-10 h-10 flex items-center justify-center rounded-full border border-gray-900 dark:border-black mr-10
-       dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-300 transform hover:scale-105`}
+       dark:bg-gray-300 bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-300 transform hover:scale-105`}
     >
       {theme === "dark" ? "☀️" : "🌙"}
     </button>
