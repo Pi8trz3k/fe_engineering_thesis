d5613d622f9561dabf708fdbf815e8de427170d8
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-05-01 09:22:16 +0200

    LandingPage with test content

--- src/pages/anon/LandingPage.tsx
+++ src/pages/anon/LandingPage.tsx
@@ -1,7 +1,21 @@
+import { useAuth } from "@/hooks/useAuth.tsx";
+
 export default function LandingPage() {
+  const { role } = useAuth();
+
+  const func = () => {
+    return (
+      <>
+        {role === "user" && <div>User się zalogował</div>}
+        {role === "admin" && <div>Admin się zalogował</div>}{" "}
+      </>
+    );
+  };
+
   return (
     <div>
       <div className={"text-black dark:text-white dark:bg-gray-800"}>Test</div>
+      {func()}
     </div>
   );
 }
