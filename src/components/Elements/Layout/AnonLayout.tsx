40d4566e93a69b1f6f972348cd4215c16b4ed55e
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-05-03 12:11:22 +0200

    Changing bg color in AnonLayout

--- src/components/Elements/Layout/AnonLayout.tsx
+++ src/components/Elements/Layout/AnonLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AnonLayout() {
   return (
-    <div className="flex h-full w-full min-h-screen bg-gray-100 dark:bg-gray-800 text-text-light dark:text-text-dark">
+    <div className="flex h-full w-full min-h-screen bg-gray-200 dark:bg-gray-800 text-text-light dark:text-text-dark">
       <main className="flex flex-col items-center justify-center h-full w-full p-5">
         <Outlet />
       </main>
