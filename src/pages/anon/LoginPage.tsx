bd19f67be865b7a072ae603f90279b2fa9187af1
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-05-05 21:01:06 +0200

    Removing unused imports

--- src/pages/anon/LoginPage.tsx
+++ src/pages/anon/LoginPage.tsx
@@ -1,5 +1,3 @@
-import { useAuth } from "@/hooks/useAuth.tsx";
-import { Link, useNavigate } from "react-router-dom";
 import LoginForm from "@/components/Elements/Forms/LoginForm.tsx";
 
 export default function LoginPage() {
