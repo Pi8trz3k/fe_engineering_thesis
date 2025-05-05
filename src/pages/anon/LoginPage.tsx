db16a406c6b7be85686b2f69e719bdc86d6a2103
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-05-05 21:14:14 +0200

    Adding title to LoginPage

--- src/pages/anon/LoginPage.tsx
+++ src/pages/anon/LoginPage.tsx
@@ -1,5 +1,10 @@
 import LoginForm from "@/components/Elements/Forms/LoginForm.tsx";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <title>BeFit.Always - zaloguj się</title>
+      <LoginForm />
+    </>
+  );
 }
