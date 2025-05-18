c1c4b04db35b3b96691b298f90d538e56b29d78b
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-05-18 19:17:27 +0200

    Adding StatCard

---
+++ src/components/Elements/Cards/StatCard.tsx
@@ -0,0 +1,13 @@
+interface statCardProps {
+  cardText: string;
+  value: string | number;
+}
+
+export default function StatCard({ cardText, value }: statCardProps) {
+  return (
+    <div className="p-10 bg-white dark:bg-gray-300 rounded-md shadow-lg text-center">
+      <h3 className="text-lg font-semibold">{cardText}</h3>
+      <p className="text-2xl font-semibold text-green-600">{value}</p>
+    </div>
+  );
+}
