3fd22e53cad35a87effe71e2c4b345b39b569ec9
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-05-18 20:33:18 +0200

    Changing AdminPanel to use the useData hook

--- src/pages/admin/AdminPanel.tsx
+++ src/pages/admin/AdminPanel.tsx
@@ -1,35 +1,16 @@
 import StatCard from "@/components/Elements/Cards/StatCard.tsx";
-import { useEffect, useState } from "react";
-import api from "@/lib/api";
-import { toast } from "react-toastify";
+import { UseData } from "@/hooks/useData.tsx";
 
 export default function AdminPanel() {
-  const [usersCount, setUsersCount] = useState(0);
-  const [trainersCount, setTrainersCount] = useState(0);
-  const [adminsCount, setAdminsCount] = useState(0);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await api.get("/user/get-all-users");
-        setUsersCount(response.data.users);
-        setTrainersCount(response.data.trainers);
-        setAdminsCount(response.data.admins);
-      } catch (error) {
-        toast.error("Wystąpił błąd");
-        console.error(error);
-      }
-    };
-    fetchData();
-  });
+  const { data } = UseData();
 
   return (
     <>
       <title>BeFit.Always - panel administratora</title>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-x-15 gap-y-5 pt-10">
-        <StatCard cardText="Użytkownicy" value={usersCount} />
-        <StatCard cardText="Trenerzy" value={trainersCount} />
-        <StatCard cardText="Administratorzy" value={adminsCount} />
+        <StatCard cardText="Użytkownicy" value={data.admin?.usersCount} />
+        <StatCard cardText="Trenerzy" value={data.admin?.trainersCount} />
+        <StatCard cardText="Administratorzy" value={data.admin?.adminsCount} />
       </div>
     </>
   );
