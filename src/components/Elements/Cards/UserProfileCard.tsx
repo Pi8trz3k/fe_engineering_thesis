3d95c1dbd9d7f50595b6a23ee938570d0a7d4718
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-05-26 23:12:05 +0200

    Moving columns to UserProfileCard component, adding actions column with icons

--- src/components/Elements/Cards/UserProfileCard.tsx
+++ src/components/Elements/Cards/UserProfileCard.tsx
@@ -1,87 +1,139 @@
 import { FrontendUser } from "@/providers/DataTypes/DataProviderTypes.ts";
 import { Table, TableColumnsType, Tooltip } from "antd";
-import { CheckCircleTwoTone, CloseCircleTwoTone } from "@ant-design/icons";
-
-const columns: TableColumnsType<FrontendUser> = [
-  {
-    title: "ID",
-    dataIndex: "userID",
-    key: "userID",
-    defaultSortOrder: "ascend",
-    sorter: (a, b) => a.userID - b.userID,
-  },
-  {
-    title: "Imię i nazwisko",
-    key: "fullName",
-    render: (_, record) => `${record.name} ${record.lastName}`,
-    sorter: (a, b) => {
-      const fullNameA = `${a.name} ${a.lastName}`.toLowerCase();
-      const fullNameB = `${b.name} ${b.lastName}`.toLowerCase();
-      return fullNameA.localeCompare(fullNameB);
-    },
-  },
-  {
-    title: "Email (weryfikacja)",
-    render: (_, record) => (
-      <div className="flex items-center gap-2">
-        <span>{record.email}</span>
-        <Tooltip
-          title={
-            record.isMailVerified
-              ? "Email potwierdzony"
-              : "Email niepotwierdzony"
-          }
-        >
-          {record.isMailVerified ? (
-            <CheckCircleTwoTone twoToneColor="#52c41a" />
-          ) : (
-            <CloseCircleTwoTone twoToneColor="#ff4d4f" />
-          )}
-        </Tooltip>
-      </div>
-    ),
-    filters: [
-      { text: "Potwierdzony", value: true },
-      { text: "Niepotwierdzony", value: false },
-    ],
-    onFilter: (_, record) => record.isMailVerified === _,
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Telefon",
-    dataIndex: "phoneNumber",
-    key: "phoneNumber",
-  },
-  {
-    title: "Typ konta",
-    dataIndex: "type",
-    key: "type",
-    render: (_, record) => (
-      <div>
-        {record.isAdmin ? (
-          <span>Administrator</span>
-        ) : record.type === "trainer" ? (
-          <span>Trener</span>
-        ) : (
-          <span>Użytkownik</span>
-        )}
-      </div>
-    ),
-    filters: [
-      { text: "Administrator", value: "administrator" },
-      { text: "Użytkownik", value: "user" },
-      { text: "Trener", value: "trainer" },
-    ],
-  },
-  {
-    title: "Uprawnienia admina",
-    dataIndex: "isAdmin",
-    key: "isAdmin",
-  },
-];
+import {
+  CheckCircleTwoTone,
+  CloseCircleTwoTone,
+  DeleteOutlined,
+  StopOutlined,
+  UserSwitchOutlined,
+} from "@ant-design/icons";
+import { deleteUser } from "@/utils/adminUtils.tsx";
+import { UseData } from "@/hooks/useData.tsx";
 
 export default function UserProfileCard({ user }: { user: FrontendUser[] }) {
+  const { fetchData } = UseData();
+
+  const columns: TableColumnsType<FrontendUser> = [
+    {
+      title: "ID",
+      dataIndex: "userID",
+      key: "userID",
+      defaultSortOrder: "ascend",
+      sorter: (a, b) => a.userID - b.userID,
+    },
+    {
+      title: "Imię i nazwisko",
+      key: "fullName",
+      render: (_, record) => `${record.name} ${record.lastName}`,
+      sorter: (a, b) => {
+        const fullNameA = `${a.name} ${a.lastName}`.toLowerCase();
+        const fullNameB = `${b.name} ${b.lastName}`.toLowerCase();
+        return fullNameA.localeCompare(fullNameB);
+      },
+    },
+    {
+      title: "Email (weryfikacja)",
+      render: (_, record) => (
+        <div className="flex items-center gap-2">
+          <span>{record.email}</span>
+          <Tooltip
+            title={
+              record.isMailVerified
+                ? "Email potwierdzony"
+                : "Email niepotwierdzony"
+            }
+          >
+            {record.isMailVerified ? (
+              <CheckCircleTwoTone twoToneColor="#52c41a" />
+            ) : (
+              <CloseCircleTwoTone twoToneColor="#ff4d4f" />
+            )}
+          </Tooltip>
+        </div>
+      ),
+      filters: [
+        { text: "Potwierdzony", value: true },
+        { text: "Niepotwierdzony", value: false },
+      ],
+      onFilter: (_, record) => record.isMailVerified === _,
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Telefon",
+      dataIndex: "phoneNumber",
+      key: "phoneNumber",
+    },
+    {
+      title: "Typ konta",
+      dataIndex: "type",
+      key: "type",
+      render: (_, record) => (
+        <div>
+          {record.isAdmin ? (
+            <span>Administrator</span>
+          ) : record.type === "trainer" ? (
+            <span>Trener</span>
+          ) : (
+            <span>Użytkownik</span>
+          )}
+        </div>
+      ),
+      filters: [
+        { text: "Administrator", value: "administrator" },
+        { text: "Użytkownik", value: "user" },
+        { text: "Trener", value: "trainer" },
+      ],
+    },
+    {
+      title: "Zarządzaj",
+      key: "actions",
+      render: (_, record) => (
+        <span className="flex items-center gap-2">
+          <Tooltip title={"Usuń użytkownika"}>
+            <DeleteOutlined
+              style={{ color: "red" }}
+              onClick={() => deleteUser(record.userID, fetchData)}
+            />
+          </Tooltip>
+
+          <Tooltip
+            title={
+              record.status === "blocked"
+                ? "Odblokuj użytkownika"
+                : record.status === "active"
+                  ? "Zablokuj użytkownika"
+                  : "Użytkownik nieaktywny"
+            }
+          >
+            <StopOutlined
+              style={{
+                color:
+                  record.status === "blocked"
+                    ? "red"
+                    : record.status === "active"
+                      ? "green"
+                      : "grey",
+              }}
+            />
+          </Tooltip>
+
+          <Tooltip
+            title={
+              record.isAdmin
+                ? "Odbierz uprawnienia admina"
+                : "Nadaj uprawnienia admina"
+            }
+          >
+            <UserSwitchOutlined
+              style={{ color: record.isAdmin ? "green" : "red" }}
+            />
+          </Tooltip>
+        </span>
+      ),
+    },
+  ];
+
   return (
     <div className="overflow-x-auto">
       <Table
