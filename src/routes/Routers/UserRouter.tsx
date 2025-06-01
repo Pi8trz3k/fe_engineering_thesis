e02dfcc2b076158b3fde2421ad68a9e8f29a1797
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-01 13:18:27 +0200

    Adding ErrorPage to Routers

--- src/routes/Routers/UserRouter.tsx
+++ src/routes/Routers/UserRouter.tsx
@@ -1,12 +1,19 @@
 import { Navigate, useRoutes } from "react-router-dom";
 import LandingPage from "@/pages/anon/LandingPage.tsx";
 import UserLayout from "@/components/Elements/Layout/UserLayout.tsx";
+import ErrorPage from "@/pages/ErrorPage.tsx";
 
 export default function UserRouter() {
   const userRoutes = [
     {
       path: "/",
       element: <UserLayout />,
+      errorElement: (
+        <ErrorPage
+          title="Coś poszło nie tak 😢"
+          message="Spróbuj odświeżyć stronę lub wróć później."
+        />
+      ),
       children: [
         { index: true, element: <LandingPage /> },
         { path: "*", element: <Navigate to="/" /> },
