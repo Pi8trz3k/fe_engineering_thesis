03062b65d2fdfd5c9cd0de4df48bd4500a8a28a4
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-04 22:13:32 +0200

    Adding main text and graphic in anon LandingPage

--- src/pages/anon/LandingPage.tsx
+++ src/pages/anon/LandingPage.tsx
@@ -1,4 +1,5 @@
 import { UseAuth } from "@/hooks/useAuth.tsx";
+import main_graphic from "@/assets/main-graphic.svg";
 
 export default function LandingPage() {
   const { role } = UseAuth();
@@ -7,15 +8,28 @@ export default function LandingPage() {
     return (
       <>
         {role === "user" && <div>User się zalogował</div>}
-        {role === "admin" && <div>Admin się zalogował</div>}
         {role === "trainer" && <div>Trener się zalogował</div>}
       </>
     );
   };
 
   return (
-    <div>
-      <div className={"text-black dark:text-white dark:bg-gray-800"}>Test</div>
+    <div className="flex flex-col md:flex-row items-center justify-center w-full md:w-[80%] mx-auto mt-8 pt-6">
+      <div className="md:w-1/2 w-full text-left  md:ml-10 ml-15 md:mb-0">
+        <h1 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white">
+          Bądź fit razem z nami
+          <br />
+          <span className="text-green-600">Zawsze</span>
+        </h1>
+      </div>
+
+      <div className="md:w-1/2 w-full flex justify-center">
+        <img
+          src={main_graphic}
+          alt="Zdjęcie fitness"
+          className="rounded-xl w-full max-w-md"
+        />
+      </div>
       {func()}
     </div>
   );
