c709de6f8aa4175587fc6e0aebd3abf8800e9532
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-05 18:06:39 +0200

    Adding gradient and removing unused properties in UserLayout

--- src/components/Elements/Layout/UserLayout.tsx
+++ src/components/Elements/Layout/UserLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function UserLayout() {
   return (
-    <div className="flex h-full w-full min-h-screen bg-green-100 dark:bg-red-100 text-text-light dark:text-text-dark">
+    <div className="flex h-full w-full min-h-screen bg-gradient-to-br from-green-100 via-white to-green-100 dark:from-gray-900 dark:via-gray-700 dark:to-black">
       <main className="flex flex-col items-center justify-center h-full w-full p-5">
         <Outlet />
       </main>
