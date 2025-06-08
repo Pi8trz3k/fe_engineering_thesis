9d2329fe12746b92abb628405d207a795c50ae03
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-09 00:57:34 +0200

    Adding TrainerRouter to routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import AnonRouter from "./Routers/AnonRouter.tsx";
 import AdminRouter from "./Routers/AdminRouter.tsx";
 import UserRouter from "./Routers/UserRouter.tsx";
+import TrainerRouter from "@/routes/Routers/TrainerRouter.tsx";
 import { UseAuth } from "@/hooks/useAuth.tsx";
 
 export default function AppRoutes() {
@@ -10,6 +11,8 @@ export default function AppRoutes() {
     return <AnonRouter />;
   } else if (role === "admin") {
     return <AdminRouter />;
+  } else if (role === "trainer") {
+    return <TrainerRouter />;
   }
   return <UserRouter />;
 }
