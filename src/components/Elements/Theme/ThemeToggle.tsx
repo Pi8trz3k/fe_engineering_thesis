addc0e6c11c0c1654a99ac63e8bdd950f65fbabf
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-09 00:08:18 +0200

    Adjusting mr in ThemeToggle

--- src/components/Elements/Theme/ThemeToggle.tsx
+++ src/components/Elements/Theme/ThemeToggle.tsx
@@ -21,7 +21,7 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className={`w-10 h-10 flex items-center justify-center rounded-full border border-gray-900  mr-10
+      className={`w-10 h-10 flex items-center justify-center rounded-full border border-gray-900 mr-7 md:mr-10
        dark:bg-gray-300 bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-300 transform hover:scale-105`}
     >
       {theme === "dark" ? "☀️" : "🌙"}
