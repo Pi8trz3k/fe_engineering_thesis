5a2a31dd71d1f145e6d052211234a5792fbf9b08
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-09 19:56:46 +0200

    Adding user pages to UserRouter

--- src/routes/Routers/UserRouter.tsx
+++ src/routes/Routers/UserRouter.tsx
@@ -2,6 +2,10 @@ import { Navigate, useRoutes } from "react-router-dom";
 import UserLandingPage from "@/pages/user/UserLandingPage.tsx";
 import UserLayout from "@/components/Elements/Layout/UserLayout.tsx";
 import ErrorPage from "@/pages/ErrorPage.tsx";
+import Trainings from "@/pages/user/Trainings.tsx";
+import Trainers from "@/pages/user/Trainers.tsx";
+import UserProfile from "@/pages/UserProfile.tsx";
+import Achievements from "@/pages/user/Achievements.tsx";
 
 export default function UserRouter() {
   const userRoutes = [
@@ -16,10 +20,10 @@ export default function UserRouter() {
       ),
       children: [
         { index: true, element: <UserLandingPage /> },
-        { path: "trainings", element: <UserLandingPage /> },
-        { path: "trainers", element: <UserLandingPage /> },
-        { path: "profile", element: <UserLandingPage /> },
-        { path: "achievements", element: <UserLandingPage /> },
+        { path: "trainings", element: <Trainings /> },
+        { path: "trainers", element: <Trainers /> },
+        { path: "profile", element: <UserProfile /> },
+        { path: "achievements", element: <Achievements /> },
         { path: "*", element: <Navigate to="/" /> },
       ],
     },
