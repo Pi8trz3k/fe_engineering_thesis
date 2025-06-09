89d69898781993b221fcc115835f8f0e33d2a203
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-09 20:06:33 +0200

    Moving navigation links to variable|Sidebar

--- src/components/Elements/Sidebar/Sidebar.tsx
+++ src/components/Elements/Sidebar/Sidebar.tsx
@@ -4,27 +4,38 @@ import { Link } from "react-router-dom";
 export default function Sidebar() {
   const { role } = UseAuth();
 
+  const navigationLinks = () => {
+    if (role === "user") {
+      return (
+        <>
+          <Link to="/">Panel główny</Link>
+          <Link to="/trainers">Trenerzy</Link>
+          <Link to="/trainings">Twoje treningi</Link>
+          <Link to="/achievements">Osiągnięcia</Link>
+          <Link to="/profile">Profil</Link>
+        </>
+      );
+    }
+
+    if (role === "trainer") {
+      return (
+        <>
+          <Link to="/">Panel główny</Link>
+          <Link to="/clients">Podopieczni</Link>
+          <Link to="/trainings">Twoje treningi</Link>
+          <Link to="/profile">Profil</Link>
+        </>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <>
       <aside className="hidden md:flex flex-col w-64 bg-white dark:bg-gray-800 shadow-lg dark:shadow-none p-4">
         <nav className="flex flex-col gap-4 text-gray-800 dark:text-white font-medium">
-          {role === "user" && (
-            <>
-              <Link to="/">Panel główny</Link>
-              <Link to="/trainers">Trenerzy</Link>
-              <Link to="/trainings">Twoje treningi</Link>
-              <Link to="/achievements">Osiągnięcia</Link>
-              <Link to="/profile">Profil</Link>
-            </>
-          )}
-          {role === "trainer" && (
-            <>
-              <Link to="/">Panel główny</Link>
-              <Link to="/clients">Podopieczni</Link>
-              <Link to="/trainings">Twoje treningi</Link>
-              <Link to="/profile">Profil</Link>
-            </>
-          )}
+          {navigationLinks()}
         </nav>
       </aside>
     </>
