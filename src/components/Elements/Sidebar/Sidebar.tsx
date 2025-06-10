5bc92d4d378bb2461c2a6be4235fe98bca395bb8
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-11 00:17:05 +0200

    Adding NavLink with links to the sites. Adding some variable and function to handle the usage of menu in both screen sizes

--- src/components/Elements/Sidebar/Sidebar.tsx
+++ src/components/Elements/Sidebar/Sidebar.tsx
@@ -1,18 +1,48 @@
 import { UseAuth } from "@/hooks/useAuth.tsx";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { MenuOutlined } from "@ant-design/icons";
+import { Drawer, Button } from "antd";
+import * as React from "react";
 
 export default function Sidebar() {
   const { role } = UseAuth();
+  const [isMobile, setIsMobile] = useState(false);
+  const [isSiderOpen, setIsSiderOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const NavLink = ({
+    to,
+    children,
+  }: {
+    to: string;
+    children: React.ReactNode;
+  }) => (
+    <Link
+      to={to}
+      className="text-black dark:text-white hover:text-green-600 transition-colors"
+    >
+      {children}
+    </Link>
+  );
 
   const navigationLinks = () => {
     if (role === "user") {
       return (
         <>
-          <Link to="/">Panel główny</Link>
-          <Link to="/trainers">Trenerzy</Link>
-          <Link to="/trainings">Twoje treningi</Link>
-          <Link to="/achievements">Osiągnięcia</Link>
-          <Link to="/profile">Profil</Link>
+          <NavLink to="/">Panel główny</NavLink>
+          <NavLink to="/trainers">Trenerzy</NavLink>
+          <NavLink to="/trainings">Twoje treningi</NavLink>
+          <NavLink to="/achievements">Osiągnięcia</NavLink>
+          <NavLink to="/profile">Profil</NavLink>
         </>
       );
     }
@@ -20,10 +50,10 @@ export default function Sidebar() {
     if (role === "trainer") {
       return (
         <>
-          <Link to="/">Panel główny</Link>
-          <Link to="/clients">Podopieczni</Link>
-          <Link to="/trainings">Twoje treningi</Link>
-          <Link to="/profile">Profil</Link>
+          <NavLink to="/">Panel główny</NavLink>
+          <NavLink to="/clients">Podopieczni</NavLink>
+          <NavLink to="/trainings">Twoje treningi</NavLink>
+          <NavLink to="/profile">Profil</NavLink>
         </>
       );
     }
@@ -33,11 +63,46 @@ export default function Sidebar() {
 
   return (
     <>
-      <aside className="hidden md:flex flex-col w-64 bg-white dark:bg-gray-800 shadow-lg dark:shadow-none p-4">
-        <nav className="flex flex-col gap-4 text-gray-800 dark:text-white font-medium">
-          {navigationLinks()}
-        </nav>
-      </aside>
+      {!isMobile && (
+        <aside className="fixed top-16 left-0 h-screen w-40 bg-white dark:bg-gray-700 shadow-lg p-4 dark:border-t-2 dark:border-gray-900 ">
+          <nav className="flex flex-col gap-4 text-black dark:text-white font-medium transition-color duration-100">
+            {navigationLinks()}
+          </nav>
+        </aside>
+      )}
+
+      {isMobile && (
+        <>
+          <Button
+            type="text"
+            icon={
+              <MenuOutlined style={{ color: "#6EE7B7", fontSize: "40px" }} />
+            }
+            onClick={() => setIsSiderOpen(true)}
+            className="absolute top-4 left-4 z-50 text-xl dark:text-white"
+          />
+          <Drawer
+            title={
+              <span className="text-xl font-semibold text-black">Menu</span>
+            }
+            placement="left"
+            onClick={() => setIsSiderOpen(false)}
+            open={isSiderOpen}
+            styles={{
+              header: {
+                backgroundColor: "#bbf7d0",
+              },
+              body: {
+                backgroundColor: "#dcfce7",
+              },
+            }}
+          >
+            <nav className="flex flex-col gap-4 text-black dark:text-white font-medium">
+              {navigationLinks()}
+            </nav>
+          </Drawer>
+        </>
+      )}
     </>
   );
 }
