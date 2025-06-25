176926f15d910536e210ff34ef579b561bd7a661
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-25 13:09:13 +0200

    Adding basic logic to EmailConfirmationPage

--- src/pages/EmailConfirmationPage.tsx
+++ src/pages/EmailConfirmationPage.tsx
@@ -1,8 +1,39 @@
+import { Button, Result } from "antd";
+import { useNavigate, useSearchParams } from "react-router";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+
 export default function EmailConfirmationPage() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const token = searchParams.get("token");
+
+  useEffect(() => {
+    const confirmEmail = async () => {
+      if (!token) {
+        toast.error("Wystąpił błąd podczas potwierdzenia emaila");
+        navigate("/");
+      }
+    };
+
+    confirmEmail();
+  }, [token]);
+
   return (
     <>
       <div>
-        <h1 className="dark:text-white">EmailConfiramtionPage</h1>
+        <h1 className="dark:text-white">EmailConfirmationPage</h1>
+        <p>{token}</p>
+        <Result
+          status="success"
+          title="Sukces!"
+          subTitle="Twój adres e-mail został pomyślnie potwierdzony."
+          extra={[
+            <Button type="primary" onClick={() => navigate("/")} key="home">
+              Przejdź do strony głównej
+            </Button>,
+          ]}
+        />
       </div>
     </>
   );
