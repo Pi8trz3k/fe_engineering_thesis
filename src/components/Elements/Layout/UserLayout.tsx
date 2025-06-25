02ef3cd39f24076174031be03e4b3c13c24bcbf0
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-25 18:42:55 +0200

    Updating UserLayout

--- src/components/Elements/Layout/UserLayout.tsx
+++ src/components/Elements/Layout/UserLayout.tsx
@@ -3,9 +3,9 @@ import Sidebar from "@/components/Elements/Sidebar/Sidebar.tsx";
 
 export default function UserLayout() {
   return (
-    <div className="flex h-full w-full min-h-screen bg-gradient-to-br from-green-100 via-white to-green-100 dark:from-gray-900 dark:via-gray-700 dark:to-black">
+    <div className="flex w-full min-h-screen bg-gradient-to-br from-green-100 via-white to-green-100 dark:from-gray-900 dark:via-gray-700 dark:to-black">
       <Sidebar />
-      <main className="flex flex-col items-center justify-center h-full w-full p-5">
+      <main className="flex flex-col flex-1 p-5 pt-20 md:pl-44">
         <Outlet />
       </main>
     </div>
