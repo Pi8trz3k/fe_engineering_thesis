5f27393290fa9fb2b172e96d82c58e9ca4f2f6ad
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-28 19:50:08 +0200

    Adding proper props to TrainerCard component

--- src/components/Elements/Cards/TrainerCard/TrainerCard.tsx
+++ src/components/Elements/Cards/TrainerCard/TrainerCard.tsx
@@ -1,22 +1,36 @@
 import image from "@/pages/user/image.jpg";
 
-export default function TrainerCard() {
+type TrainerCardProps = {
+  name: string;
+  lastName: string;
+  meanOfStars: number | null;
+  opinionsCount: number | null;
+};
+
+export default function TrainerCard({
+  name,
+  lastName,
+  meanOfStars,
+  opinionsCount,
+}: TrainerCardProps) {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-md p-4 transition-transform hover:scale-105">
+    <div className="bg-white dark:bg-gray-700 rounded-2xl shadow-md p-4 transition-transform hover:scale-105">
       <img
         src={image}
         alt="trener image"
         className="rounded-xl h-40 w-full object-cover"
       />
       <h3 className="mt-3 text-lg font-semibold text-gray-900 dark:text-white">
-        Jan Kowalski
+        {name} {lastName}
       </h3>
       <p className="text-sm text-gray-600 dark:text-gray-300">
         Specjalizacja: Odchudzanie
       </p>
       <div className="flex justify-between items-center mt-4">
         <span className="text-green-600 dark:text-green-400 text-sm">
-          ⭐ 4.9 (34 opinie)
+          {opinionsCount
+            ? `⭐ ${meanOfStars} (Opinie: ${opinionsCount})`
+            : `Brak opinii`}
         </span>
         <button className="text-sm text-white bg-green-500 px-3 py-1 rounded hover:bg-green-600">
           Zobacz profil
