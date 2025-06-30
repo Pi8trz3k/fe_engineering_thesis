2002080eb96c48bfdbc8dc60cd923713f4bfc7f4
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-06-30 20:29:54 +0200

    Changing pt in UserLayout

--- src/components/Elements/Layout/UserLayout.tsx
+++ src/components/Elements/Layout/UserLayout.tsx
@@ -5,7 +5,7 @@ export default function UserLayout() {
   return (
     <div className="flex w-full min-h-screen bg-gradient-to-br from-green-100 via-white to-green-100 dark:from-gray-900 dark:via-gray-800 dark:to-black">
       <Sidebar />
-      <main className="flex flex-col flex-1 p-5 pt-20 md:pl-44">
+      <main className="flex flex-col flex-1 p-5 pt-10 md:pl-44">
         <Outlet />
       </main>
     </div>
