cbb656c207b17f7afabb2325067504477db6cd80
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-02 13:49:56 +0200

    Adding trainerTypes and useTrainers with fetchTrainers in TrainersPageData

--- src/utils/TrainersPageData.tsx
+++ src/utils/TrainersPageData.tsx
@@ -4,13 +4,15 @@ import { useState } from "react";
 
 export const useFilterData = () => {
   const [cities, setCities] = useState([]);
-  // const [trainerTypes, setTrainerTypes] = useState([]);
+  const [trainerTypes, setTrainerTypes] = useState([]);
 
   const fetchFilterData = async () => {
     try {
       const citiesResponse = await api.get(`/city`);
+      const trainerTypesResponse = await api.get(`/trainer-type`);
 
       setCities(citiesResponse.data);
+      setTrainerTypes(trainerTypesResponse.data);
     } catch (error: any) {
       console.error(error);
       toast.error("Wystąpił błąd podczas pobierania danych");
@@ -19,7 +21,36 @@ export const useFilterData = () => {
 
   return {
     cities,
-    // trainerTypes,
+    trainerTypes,
     fetchFilterData,
   };
+};
+
+export const useTrainers = () => {
+  const [trainers, setTrainers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalCount, setTotalCount] = useState(0);
+  const PAGE_SIZE = 12;
+
+  const fetchTrainers = async (page: number) => {
+    setLoading(true);
+
+    const skip = (page - 1) * PAGE_SIZE;
+
+    try {
+      const trainersResponse = await api.get(
+        `/trainer?skip=${skip}&limit=${PAGE_SIZE}`,
+      );
+      console.log(trainersResponse);
+      setTrainers(trainersResponse.data);
+      setTotalCount(trainersResponse.data.length);
+    } catch (error: any) {
+      console.error(error);
+      toast.error("Wystąpił błąd podczas pobierania danych");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { trainers, loading, totalCount, fetchTrainers, PAGE_SIZE };
 };
