f4440e266e88f16c798be6b9fa73d0ba95d92ff0
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-06 18:58:36 +0200

    Changes in TrainerProfilePage.tsx

--- src/pages/user/TrainerProfilePage.tsx
+++ src/pages/user/TrainerProfilePage.tsx
@@ -1,7 +1,25 @@
-export default function TrainerProfilePage() {
+import emptyProfileCard from "@/assets/emptyProfileCard.avif";
+import { TrainerProfilePageProps } from "@/pages/user/DataTypes/TrainersPageTypes.ts";
+
+export default function TrainerProfilePage({
+  trainerId,
+}: TrainerProfilePageProps) {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className="relative w-[70%] aspect-[4/3] mx-auto">
+          <img
+            src={emptyProfileCard}
+            alt="trener image"
+            className="absolute inset-0 w-full h-full object-cover rounded-xl"
+            loading="lazy"
+          />
+        </div>
+        <div className="bg-green-300">ASKNDajsndajsndjasnd </div>
+        <div className="bg-yellow-300">ASKNDajsndajsndjasnd </div>
+        <div className="bg-red-300">ASKNDajsndajsndjasnd </div>
+        <div className="bg-brown-300">{trainerId} </div>
+      </div>
     </>
   );
 }
