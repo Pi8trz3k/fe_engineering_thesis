a870176b16abad0c0635d8a9163e7a0a975fef58
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-09 19:16:23 +0200

    Add TrainerPageData

---
+++ src/utils/TrainerPageData.tsx
@@ -0,0 +1,69 @@
+import api from "@/lib/api.tsx";
+import { toast } from "react-toastify";
+import { useState } from "react";
+import {
+  OpinionBackend,
+  TrainerBackend,
+} from "@/pages/user/DataTypes/TrainersPageTypes.ts";
+
+export const useOpinions = () => {
+  const [opinions, setOpinions] = useState<OpinionBackend>();
+  const [opinionsLoading, setOpinionsLoading] = useState<boolean>(true);
+  const [totalCount, setTotalCount] = useState<number>(0);
+  const OPINION_PAGE_SIZE = 12;
+
+  const fetchOpinions = async (trainerId: number, page: number) => {
+    setOpinionsLoading(true);
+
+    const skip = (page - 1) * OPINION_PAGE_SIZE;
+    const params = {
+      trainer_id: trainerId,
+      skip,
+      limit: OPINION_PAGE_SIZE,
+    };
+
+    try {
+      const opinionsResponse = await api.get(`/opinion`, { params });
+      setOpinions(opinionsResponse.data.opinions);
+      setTotalCount(opinionsResponse.data.total_count);
+      console.log("opinions: ", opinions);
+    } catch (error: any) {
+      console.error(error);
+      toast.error("Wystąpił błąd podczas pobierania danych");
+    } finally {
+      setOpinionsLoading(false);
+    }
+  };
+
+  return {
+    opinions,
+    totalCount,
+    loading: opinionsLoading,
+    fetchOpinions,
+  };
+};
+
+export const useTrainer = () => {
+  const [trainer, setTrainer] = useState<TrainerBackend>();
+  const [trainerLoading, setTrainerLoading] = useState<boolean>(false);
+
+  const fetchTrainer = async (trainerId: number) => {
+    setTrainerLoading(true);
+
+    try {
+      const trainerResponse = await api.get(`/trainer/${trainerId}`);
+      setTrainer(trainerResponse.data.trainer);
+
+      if (trainerResponse.data.length === 0) {
+        toast.info("Nie znaleziono danych");
+      }
+    } catch (error: any) {
+      console.error(error);
+      toast.error("Wystąpił błąd podczas pobierania danych");
+    } finally {
+      setTrainerLoading(false);
+    }
+  };
+
+  return { trainer, trainerLoading, fetchTrainer };
+};
