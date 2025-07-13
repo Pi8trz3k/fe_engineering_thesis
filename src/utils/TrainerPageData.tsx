4f66450a146993846faca2238646e5002e077cce
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-13 18:19:20 +0200

    Adding usePicture to TrainerPageData.tsx

--- src/utils/TrainerPageData.tsx
+++ src/utils/TrainerPageData.tsx
@@ -5,6 +5,7 @@ import {
   OpinionBackend,
   TrainerBackend,
 } from "@/pages/user/DataTypes/TrainersPageTypes.ts";
+import emptyProfileCard from "@/assets/emptyProfileCard.avif";
 
 export const useOpinions = () => {
   const [opinions, setOpinions] = useState<OpinionBackend[]>([]);
@@ -66,4 +67,33 @@ export const useTrainer = () => {
   };
 
   return { trainer, trainerLoading, fetchTrainer };
+};
+
+export const usePicture = () => {
+  const [img, setImg] = useState<string>();
+  const [imgLoading, setImgLoading] = useState<boolean>(false);
+
+  const fetchImage = async (trainerId: number) => {
+    setImgLoading(true);
+
+    try {
+      const imageResponse = await api.get(`/file/${trainerId}`, {
+        responseType: "blob",
+      });
+      const imageBlob = await imageResponse.data;
+      const imageObjectURL = URL.createObjectURL(imageBlob);
+      setImg(imageObjectURL);
+    } catch (error: any) {
+      if (error.response?.status === 404) {
+        setImg(emptyProfileCard);
+      } else {
+        console.error(error);
+        toast.error("Wystąpił błąd podczas pobierania danych");
+      }
+    } finally {
+      setImgLoading(false);
+    }
+  };
+
+  return { img, imgLoading, fetchImage };
 };
