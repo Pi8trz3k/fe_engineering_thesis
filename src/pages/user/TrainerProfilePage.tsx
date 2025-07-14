4a381f75c77cc93070e09a0fb8cad3f0c6c64b53
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-15 00:04:12 +0200

    Add button and form to send students data

--- src/pages/user/TrainerProfilePage.tsx
+++ src/pages/user/TrainerProfilePage.tsx
@@ -3,7 +3,7 @@ import {
   TrainerProfilePageProps,
 } from "@/pages/user/DataTypes/TrainersPageTypes.ts";
 import { useEffect, useState } from "react";
-import { Pagination, Spin } from "antd";
+import { Pagination, Spin, Input, Button, message } from "antd";
 import OpinionCard from "@/components/Elements/Cards/OpinionCard/OpinionCard.tsx";
 import {
   useOpinions,
@@ -25,6 +25,7 @@ export default function TrainerProfilePage({
   const { img, imgLoading, fetchImage } = usePicture();
   const [currentOpinionsPage, setCurrentOpinionsPage] = useState(1);
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+  const [messageToTrainer, setMessageToTrainer] = useState("");
 
   useEffect(() => {
     fetchTrainer(trainerId);
@@ -38,19 +39,26 @@ export default function TrainerProfilePage({
     fetchImage(trainerId);
   }, [trainerId]);
 
+  const handleSubmit = async () => {
+    if (!messageToTrainer.trim()) {
+      message.warning("Wiadomość nie może być pusta.");
+      return;
+    }
+  };
+
   return trainerLoading ? (
     <div className="flex justify-center items-center h-96">
       <Spin size="large" />
     </div>
   ) : (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 dark:text-white">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 dark:text-white mt-6">
         {imgLoading ? (
-          <div className="flex justify-center items-center h-96">
+          <div className="flex justify-center items-center h-96 col-span-1 sm:col-span-1">
             <Spin size="large" />
           </div>
         ) : (
-          <div className="relative w-[65%] aspect-[4/3] mx-auto">
+          <div className="relative w-[90%] aspect-[4/3] mx-auto">
             <img
               src={img}
               alt="Trainer image"
@@ -60,30 +68,58 @@ export default function TrainerProfilePage({
           </div>
         )}
 
-        <div>
-          <div className="font-bold">
-            {trainer?.name} {trainer?.last_name} (⭐ {trainer?.mean_of_stars})
+        <div className="flex flex-col gap-4 px-2">
+          <div className="text-xl font-bold">
+            {trainer?.name} {trainer?.last_name}
+            <span className="ml-2 text-yellow-500">
+              ⭐ {trainer?.mean_of_stars}
+            </span>
           </div>
-          <div>{trainer?.description || "Brak opisu"}</div>
-        </div>
-        <div>
+
           <div>
-            <a className="font-semibold">Dostępne miasta: </a>
-            {[...new Set(trainer?.locations?.map((loc) => loc.city))].join(
-              ", ",
-            ) || "Brak miast"}
+            <p className="font-semibold">Opis:</p>
+            <p className="italic text-gray-500 dark:text-gray-300">
+              {trainer?.description || "Brak opisu"}
+            </p>
           </div>
+
           <div>
-            {" "}
-            <span className="font-semibold">Specjalizacje: </span>
-            {trainer?.types?.join(", ") || "Brak specjalizacji"}
+            <p className="font-semibold">Dostępne miasta:</p>
+            <p>
+              {[...new Set(trainer?.locations?.map((loc) => loc.city))].join(
+                ", ",
+              ) || "Brak miast"}
+            </p>
+          </div>
+
+          <div>
+            <p className="font-semibold">Specjalizacje:</p>
+            <p>{trainer?.types?.join(", ") || "Brak specjalizacji"}</p>
           </div>
         </div>
-        <div className="bg-red-300">ASKNDajsndajsndjasnd </div>
       </div>
-      <p className="mt-5 mb-5 font-semibold dark:text-white">Opinie:</p>
+      <div className="mt-8 w-[95%] md:w-[50%] m-auto dark:text-white ">
+        <a>Zapytaj o współpracę, wyślij wiadomość do trenera: </a>
+        <Input.TextArea
+          value={messageToTrainer}
+          onChange={(e) => setMessageToTrainer(e.target.value)}
+          rows={5}
+          placeholder="Witam,szukam trenera personalnego do współpracy, który pomoże mi..."
+        />
+        <div className="flex justify-center mt-3">
+          <Button
+            type="primary"
+            onClick={handleSubmit}
+            className="bg-green-500 text-white w-[200px]"
+          >
+            Wyślij wiadomość
+          </Button>
+        </div>
+      </div>
+
+      <p className="mt-10 mb-5 font-semibold dark:text-white">Opinie:</p>
       {opinionsLoading ? (
-        <div className="flex justify-center my-10">
+        <div className="flex my-10">
           <Spin size="large" />
         </div>
       ) : (
