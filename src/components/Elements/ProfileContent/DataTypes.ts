34db95752bb3d2b49626d44331f31ec5ef15c784
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-16 18:54:07 +0200

    Add new data type

--- src/components/Elements/ProfileContent/DataTypes.ts
+++ src/components/Elements/ProfileContent/DataTypes.ts
@@ -14,3 +14,14 @@ export type UserBackend = {
 export type UserProfileContentProps = {
   user?: UserBackend;
 };
+
+export type CommonProfileContentProps = {
+  user_id: number;
+  name: string;
+  last_name: string;
+  phone_number: string;
+  email: string;
+  password: string;
+  is_mail_verified: boolean;
+  status: string;
+};
