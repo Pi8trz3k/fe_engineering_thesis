2c20f72623f8fc49c1e4df038567c8e55c85cec0
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-17 00:28:13 +0200

    Edit TrainerProfileContent.tsx

--- src/components/Elements/ProfileContent/TrainerProfileContent.tsx
+++ src/components/Elements/ProfileContent/TrainerProfileContent.tsx
@@ -1,7 +1,38 @@
-export default function TrainerProfileContent() {
+import {
+  TrainerBackendContent,
+  TrainerProfileContentProps,
+} from "@/components/Elements/ProfileContent/DataTypes.ts";
+import { useEffect, useState } from "react";
+import api from "@/lib/api.tsx";
+import { toast } from "react-toastify";
+
+export default function TrainerProfileContent({
+  trainerId,
+}: TrainerProfileContentProps) {
+  const [trainer, setTrainer] = useState<TrainerBackendContent | null>(null);
+
+  useEffect(() => {
+    if (!trainerId) return;
+
+    const fetchActualTrainer = async () => {
+      try {
+        const response = await api.get(`/trainer/${trainerId}`);
+        console.log("response", response.data);
+        setTrainer(response.data);
+      } catch (error: any) {
+        toast.error("Wystąpił błąd podczas pobierania danych!");
+        console.log(error);
+      }
+    };
+
+    fetchActualTrainer();
+  }, []);
+
   return (
     <>
-      <div>Trainer Profile Content</div>
+      <div>Desc: {trainer?.description}</div>
+      {/*<div>Desc: {trainer?.locations}</div>*/}
+      <div>Types: {trainer?.types}</div>
     </>
   );
 }
