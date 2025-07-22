a734f4e5bd58ebe0d523556c9bbc02958c783215
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-22 19:32:56 +0200

    Add TrainersRequest to user sidebar

--- src/components/Elements/Sidebar/Sidebar.tsx
+++ src/components/Elements/Sidebar/Sidebar.tsx
@@ -41,6 +41,7 @@ export default function Sidebar() {
           <Link to="/">Panel główny</Link>
           <Link to="/trainers">Trenerzy</Link>
           <Link to="/trainings">Twoje treningi</Link>
+          <Link to="/requests">Prośby do trenerów </Link>
           <Link to="/achievements">Osiągnięcia</Link>
           <Link to="/profile">Profil</Link>
         </>
