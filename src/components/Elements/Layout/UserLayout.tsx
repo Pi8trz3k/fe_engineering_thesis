149d9ac9d96a1789da69e89c59b1db27ddf09a0f
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-23 12:20:59 +0200

    Repair user layout

--- src/components/Elements/Layout/UserLayout.tsx
+++ src/components/Elements/Layout/UserLayout.tsx
@@ -5,7 +5,7 @@ export default function UserLayout() {
   return (
     <div className="flex h-full w-full min-h-screen bg-gradient-to-br from-green-100 via-white to-green-100 dark:from-gray-900 dark:via-gray-800 dark:to-black">
       <Sidebar />
-      <main className="flex flex-col flex-1 h-full w-full sm:pr-5 sm:px-5 pt-10 md:pl-44">
+      <main className="flex flex-col flex-1 min-w-0 h-full w-full sm:pr-5 sm:px-5 pt-10 md:pl-44">
         <Outlet />
       </main>
     </div>
