33f6054000bc2c4041d588266624a23301dde366
Pi8trz3k <pietrzak2001kacper@o2.pl>
2025-07-28 21:23:52 +0200

    Adjust Trainings with getting userId

--- src/pages/user/Trainings.tsx
+++ src/pages/user/Trainings.tsx
@@ -1,9 +1,25 @@
 import { UseAuth } from "@/hooks/useAuth.tsx";
 import UserTrainings from "@/pages/user/UserTrainings.tsx";
 import TrainerTrainings from "@/pages/trainer/TrainerTrainings.tsx";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export default function Trainings() {
-  const { role } = UseAuth();
+  const { role, accessToken } = UseAuth();
+  const [userId, setUserId] = useState<number>();
 
-  return role === "user" ? <UserTrainings /> : <TrainerTrainings />;
+  useEffect(() => {
+    if (accessToken) {
+      const decoded = jwtDecode<{ user_id: number }>(accessToken);
+      setUserId(decoded.user_id);
+    }
+  }, [accessToken]);
+
+  if (userId === undefined) return <a>Ładowanie treningów</a>;
+
+  return role === "user" ? (
+    <UserTrainings userId={userId} />
+  ) : (
+    <TrainerTrainings />
+  );
 }
